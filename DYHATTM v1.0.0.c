f3a3ee7a51df316344849410265456a2e3070931
KyabiaWilks <166302937+KyabiaWilks@users.noreply.github.com>
2024-04-10 22:39:37 +0800

    Commit DoYouHaveAnythingToTellMe vol.1
    The program isn't finished, yet my homework make me out of time to complete it. I'm sure you all have the picture of cyll's smile. Do it your self and do it for fun.
    Communication is welcomed.

---
+++ DYHATTM v1.0.0.c
@@ -0,0 +1,28 @@
+
+                                            #include<stdio.h>/*i don't want to qixiu~*/
+                                /* Made by No one, in a good class, good afternoon, 24.4.3*/
+                              /* Hello reader! this is a project made by a heart-broken fkFDS*/
+                            #include <time.h>/*student. I would try my best to make the code be*/
+                           #include <stdlib.h>/* more interesting so I write something here. if I */
+                          void cyfwrite(int cyll) {/* get tired and can't finish this work feel free */ 
+                         FILE *qixiu = NULL;/* to take my job! let's create something more meaningful */
+                        qixiu = fopen("cyll.txt", "w+");/*than the reports and peer reviews in FDS cour */
+                        int chenyue_smile_painting_int/*se. I don't want to qixiu____===^^^^^^^===ne take*/
+                       for(int i = 0;i < cyll; i++){//```         ```^^^^^```````                 /*ok,this*/
+                       fprintf(qixiu, "Do you have ");
+                       fprintf(qixiu, "anything ");                                                     /*om*/
+                      fprintf(qixiu, "to tell");                                                         /*cy*/
+                      fprintf(qixiu, " me?");//                                                             smi
+                      /* is a for, wow----= */                                                               //l          
+                      }/* this line is a go */                                                                  
+                      
+                                                                                                              //e
+                      fclose(qixiu);// gooood                                                 __________      \\                                                       
+                      return; }// i m not-            _____============                  -=================   ||
+                     void yourDeadTime(              /*============``````                 ````````````````    */
+                     int Is, int Now) {//               ________________                    _____________      |\-
+                     /*let me ||||--            ___---====================-----_______--=================---___\*/
+                     while(Now > 0)//{ --------//   -/  #########       \\    //     \=\\   #######    \   |   |/
+                     Now--; Is --;//  `````````||    \  #######     __  \\  ///       \=\\\_ #####    //   \  |/   
+                if(Now==Is)return;//            |      -----=====^//        /           = \^^--------===^^  ||/
+                }          //I think             \_________________________/             ==\________________//
